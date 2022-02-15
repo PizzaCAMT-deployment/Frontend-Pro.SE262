bd5de945654a875158754d1b4cb11f59b07014b3
mikeJT <jeerasaktarat@gmail.com>
2022-02-15 21:38:45 +0700

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 const port = 3000;
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/view/index.html");
 });
 
 app.listen(port, function(){
