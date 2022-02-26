7b4e5c19d0ecc3dc4964facddf3bdd3f6bdc4c54
mikeJT <jeerasaktarat@gmail.com>
2022-02-26 20:42:15 +0700

    changed image to img and customer home page done version-0.1.1

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const app = express();
-
 const port = 4040;
 
 //static to called CSS
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/css'))
+app.use('/img', express.static(__dirname + '/public/img'))
 
 // Set views 
 app.set('views', './views/Customers');
